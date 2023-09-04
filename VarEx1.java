30f9f5912e6468a3a23a6f269ea35c186ba15d28
Shastrakar29 <144015433+Shastrakar29@users.noreply.github.com>
2023-09-04 16:54:20 +0530

    Create VarEx1.java
    One file added

---
+++ VarEx1.java
@@ -0,0 +1,14 @@
+class varEx1 {
+    public static void main(String[] args) {
+        int rollno;
+        rollno =10;
+        int age= 21;
+        double price=105.55;
+        Boolean isfree=true;
+        
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isfree);
+        
+    }
+}
